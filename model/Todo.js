b9d150b538bd3e4858958de480481056ea8f5800
jbpcasoy <72642782+jbpcasoy@users.noreply.github.com>
2022-11-15 19:35:55 +0800

    implemented post

---
+++ model/Todo.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const todoSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    deadline: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+module.exports = Todo;
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Todo = require("./model/Todo");
+mongoose.connect("mongodb://localhost:27017/todo");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   return res.send("Hello World");
@@ -8,6 +14,18 @@ app.get("/", (req, res) => {
 
 app.get("/another", (req, res) => {
   return res.send("Another Hello World");
+});
+
+app.post("/todo", async function (req, res) {
+  try {
+    await Todo.validate(req.body);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+
+  const todo = await Todo.create(req.body);
+
+  return res.status(201).json(todo);
 });
 
 app.listen(3000);
